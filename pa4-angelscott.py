4cd503549856dd198399bc8b2448ebdbdbd8d0b9
angel scott <ascott1@loyola.edu>
2021-11-10 19:58:08 -0500

    finished code

--- pa4-angelscott.py
+++ pa4-angelscott.py
@@ -11,7 +11,7 @@ import random
 
 
 # Function to make a general deck of cards and shuffle it. Returns a list of strings representing cards.
-def deckofcards():
+def deck_of_cards():
     suits = ["c", "h", "d", "s"]
     numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]
     deck = []
@@ -24,97 +24,97 @@ def deckofcards():
 
 
 # Function to convert a list of card strings to a list card names (number and suit). Returns a list of cards.
-def cardname(list):
+def card_name(list):
     x = 0
     while x <= 52:
         for i in list:
             card = list.pop(list.index(i))
-            cardnumber, cardsuit = card.split(" ")
-            cardnumber = int(cardnumber)
-            if cardsuit == "c":
-                cardsuit = "clubs"
-            elif cardsuit == "d":
-                cardsuit = "diamonds"
-            elif cardsuit == "s":
-                cardsuit = "spades"
-            elif cardsuit == "h":
-                cardsuit = "hearts"
-            if cardnumber == 1:
-                thecard = "Ace of " + cardsuit
-            elif cardnumber == 11:
-                thecard = "Jack of " + cardsuit
-            elif cardnumber == 12:
-                thecard = "Queen of " + cardsuit
-            elif cardnumber == 13:
-                thecard = "King of " + cardsuit
+            card_number, card_suit = card.split(" ")
+            card_number = int(card_number)
+            if card_suit == "c":
+                card_suit = "clubs"
+            elif card_suit == "d":
+                card_suit = "diamonds"
+            elif card_suit == "s":
+                card_suit = "spades"
+            elif card_suit == "h":
+                card_suit = "hearts"
+            if card_number == 1:
+                the_card = "Ace of " + card_suit
+            elif card_number == 11:
+                the_card = "Jack of " + card_suit
+            elif card_number == 12:
+                the_card = "Queen of " + card_suit
+            elif card_number == 13:
+                the_card = "King of " + card_suit
             else:
-                thecard = str(cardnumber) + " of " + cardsuit
-            return thecard
+                the_card = str(card_number) + " of " + card_suit
+            return the_card
         x += 1
 
 
 # Function to convert cards to points. Returns a list of point values.
-def pointconversion(list):
+def point_conversion(list):
     x = 0
-    pointlist = []
+    point_list = []
     while x <= 52:
         for i in range(0, len(list)):
             card = list[i]
-            cardnumber, cardsuit = card.split(" of ")
-            if cardnumber.isdigit():
-                point = int(cardnumber)
-            elif cardnumber == "Ace":
+            card_number, card_suit = card.split(" of ")
+            if card_number.isdigit():
+                point = int(card_number)
+            elif card_number == "Ace":
                 point = 11
-            elif cardnumber == "Jack" or cardnumber == "Queen" or cardnumber == "King":
+            elif card_number == "Jack" or card_number == "Queen" or card_number == "King":
                 point = 10
-            pointlist.append(point)
-        return pointlist
+            point_list.append(point)
+        return point_list
     x += 1
 
 
 # Function to calculate sum of points in successive cards. Returns a list of point sums.
-def sumofpoints(list):
-    sumlist = []
+def sum_of_points(list):
+    sum_list = []
     x = 0
     b = 2
     while x <= 50:
-        thesum = sum(list[:b])
+        the_sum = sum(list[:b])
         b += 1
-        sumlist.append(thesum)
+        sum_list.append(the_sum)
         x += 1
-    return sumlist
+    return sum_list
 
 
-# Function to make a card list for the user's deck. Returns a list of cards.
-def makingthedeck():
+# Function to make a card list for the deck. Returns a list of cards.
+def making_the_deck():
     x = 1
-    thedeck = []
+    the_deck = []
     while x <= 52:
-        card = cardname(deckofcards())
-        thedeck.append(card)
+        card = card_name(deck_of_cards())
+        the_deck.append(card)
         x += 1
-    return thedeck
+    return the_deck
 
 
 # Function to find the first two cards in user's hand:
-def userhand(decklist):
-    cardsinhand = []
-    cardsinhand.extend([decklist[0], decklist[1]])
-    return cardsinhand
+def user_hand(list):
+    cards_in_hand = []
+    cards_in_hand.extend([list[0], list[1]])
+    return cards_in_hand
 
 
 # Function to output user's first two cards, and their point total:
-def userfirstcards(userhandlist, sumpointslist):
-    print("Your first card is:", userhandlist[0])
-    print("Your second card is:", userhandlist[1])
-    print("Your total points is:", sumpointslist[0])
-    return sumpointslist[0]
+def user_first_cards(handlist, sumlist):
+    print("Your first card is:", handlist[0])
+    print("Your second card is:", handlist[1])
+    print("Your total points is:", sumlist[0])
+    return sumlist[0]
 
 
 # Function to add cards to user's hand, and calculate total points until reaching or exceeding 21:
-def userhandplus(list, sum):
-    cardsinhand = []
-    cardcount = 2
+def user_hand_plus(list, sum):
+    cards_in_hand = []
+    card_count = 2
     request = input("Do you request another card? Yes or No:")
     request = request.strip().lower()
     if request != "yes" and request != "no":
@@ -127,36 +127,36 @@ def userhandplus(list, sum):
         x = 1
         count = len(sum)
         while request == "yes":
-            cardcount += 1
+            card_count += 1
             print("Your next card is:", list[i])
             if x < count:
                 print("Your total point is:", sum[x])
-                cardsinhand.append(list[i])
+                cards_in_hand.append(list[i])
                 if sum[x] != 21:
                     request = input("Do you request another card? Yes or No:")
                     request = request.strip().lower()
                 else:
                     print("You have reached 21. You have won!")
-                    return "End", list[0:cardcount]
+                    return "End", list[0:card_count]
             else:
                 print("Total has exceeded 21. You have lost.")
-                return "End", list[0:cardcount]
+                return "End", list[0:card_count]
             i += 1
             x += 1
         if request == "no":
             print("Okay. Dealer will play now.")
-            return sum[x-1], list[0:cardcount]
+            return sum[x-1], list[0:card_count]
     elif request == "no":
         print("Okay. Dealer will play now.")
-        return sum[0], list[0:cardcount]
+        return sum[0], list[0:card_count]
 
 
 # Function to create dealer's hand. Calculates total points until reaching or exceeding 17:
-def dealerhand(dealerhand, dealersum):
+def dealer_hand(dealerhand, dealersum):
     print("Dealer's first card is:", dealerhand[0])
     print("Dealer's second card is:", dealerhand[1])
     print("Dealer's total points is:", dealersum[0])
-    cardcount = 2
+    card_count = 2
     if dealersum[0] >= 17:
         print("Points will now be compared.")
         return dealersum[0], dealerhand[0:2]
@@ -167,10 +167,10 @@ def dealerhand(dealerhand, dealersum):
         while x < count:
             print("The dealer's next card is:", dealerhand[i])
             print("The dealer's total points is:", dealersum[x])
-            cardcount += 1
+            card_count += 1
             if dealersum[x] >= 17 and dealersum[x] <= 21:
                 print("The dealer has stopped requesting cards. Points will now be compared.")
-                return dealersum[x], dealerhand[0:cardcount]
+                return dealersum[x], dealerhand[0:card_count]
             elif dealersum[x] == 21:
                 print("The dealer has reached 21. Sorry. You lost.")
             elif dealersum[x] > 21:
@@ -181,70 +181,70 @@ def dealerhand(dealerhand, dealersum):
         if x >= count:
             print("The dealer's next card is:", dealerhand[i])
             print("The dealer's total points have exceeded 21. You win!")
-            return "End", dealerhand[0: cardcount]
+            return "End", dealerhand[0: card_count]
 
 
 # Game play:
 def main():
     # Creating user's deck of cards, and a list of corresponding points.
-    userdeck = makingthedeck()
-    userhandlist = userhand(userdeck)
-    userpoints = pointconversion(userdeck)
+    user_deck = making_the_deck()
+    user_hand_list = user_hand(user_deck)
+    user_points = point_conversion(user_deck)
 
     # Creating a list of point totals to use as player draws from their deck.
-    sumpointslist = sumofpoints(userpoints)
+    sum_points_list = sum_of_points(user_points)
     x = 0
     while x <= 51:
-        for i in sumpointslist:
+        for i in sum_points_list:
             if int(i) > 21:
-                sumpointslist.remove(i)
+                sum_points_list.remove(i)
         x += 1
 
     # Printing user's first cards and their total.
-    userstarterdeck = userfirstcards(userhandlist, sumpointslist)
-    if userstarterdeck == 21:
+    user_starter_deck = user_first_cards(user_hand_list, sum_points_list)
+    if user_starter_deck == 21:
         print("You won!")
-        print("Your hand:", userhandlist[0:2])
+        print("Your hand:", user_hand_list[0:2])
     else:
         # Gameplay for user
-        usergame, userfinalhand = userhandplus(userdeck, sumpointslist)
+        user_game, user_final_hand = user_hand_plus(user_deck, sum_points_list)
 
         # Checking user's status. If they did not reach or exceed 21, dealer will play.
-        if usergame == "End":
+        if user_game == "End":
             print("Game Over.")
-            print("The user's hand:", userfinalhand)
-        elif usergame != "End":
+            print("The user's hand:", user_final_hand)
+        elif user_game != "End":
             # Creating dealer's deck of cards, and a list of corresponding points.
-            dealerdeck = makingthedeck()
-            dealerpoints = pointconversion(dealerdeck)
+            dealer_deck = making_the_deck()
+            dealer_points = point_conversion(dealer_deck)
 
             # Creating a list of totals to use as the dealer draws from their deck.
-            dealersumpointslist = sumofpoints(dealerpoints)
+            dealer_sum_points_list = sum_of_points(dealer_points)
             x = 0
             while x <= 51:
-                for i in dealersumpointslist:
+                for i in dealer_sum_points_list:
                     if int(i) > 21:
-                        dealersumpointslist.remove(i)
+                        dealer_sum_points_list.remove(i)
                 x += 1
 
             # Printing dealer's hand and total until total reaches or exceeds 17.
-            dealergame, dealerfinalhand = dealerhand(dealerdeck, dealersumpointslist)
+            dealer_game, dealer_final_hand = dealer_hand(dealer_deck, dealer_sum_points_list)
 
             # Checking dealer's status. If they did not reach or exceed 21, points will be compared.
-            if dealergame == "End":
+            if dealer_game == "End":
                 print("Game Over")
             else:
                 # Comparing points. Printing the outcome.
-                if usergame > dealergame:
+                if user_game > dealer_game:
                     print("Yay! You won!")
-                elif usergame == dealergame:
+                elif user_game == dealer_game:
                     print("It was a tie.")
-                elif usergame < dealergame:
+                elif user_game < dealer_game:
                     print("Sorry. You lost.")
 
             # Printing the player's hands.
-            print("The user's hand:", userfinalhand)
-            print("The dealer's hand:", dealerfinalhand)
+            print("The user's hand:", user_final_hand)
+            print("The dealer's hand:", dealer_final_hand)
 
 
 main()
